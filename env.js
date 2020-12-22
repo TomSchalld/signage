20a7652203ea7ab2538d176851a98e78ff7cf8bc
Thomas Schalldach <tom@riddle.wtf>
2020-12-22 23:54:53 +0100

    Adding dummy/fallback env.js

---
+++ env.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    env: {
+
+        LOCAL_FOLDER: 'img',
+        REMOTE_FOLDER: '/',
+        SFTP_PROVIDER: 'ssh.example.com',
+        SFTP_PORT: 22,
+        SFTP_USER: 'sftp_user',
+        SFTP_PASSWORD: 'sftp_password',
+        ENABLE_KIOSK: false
+
+    }
+};
